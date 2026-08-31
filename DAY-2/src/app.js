e3becf997e828cfb469d45ae28c1e4a1322349c5
ByteMaster-Bhavesh <somwanshibhavesh71@gmail.com>
2026-08-31 18:35:48 +0530

    Complate fiest task of DAY-1

--- DAY-2/src/app.js
+++ DAY-2/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 module.exports = app
  
-app.use=(express.json())
+app.use(express.json())
 
 const notes = []
 
@@ -11,4 +11,35 @@ app.post("/notes", (req,res)=>{
     res.status(201).json({
         message:"Note created successfully"
     })
+})
+
+app.get("/notes",(req,res) => {
+    console.log( notes )
+    res.status(200).json({
+        message:"Successfully Recives Note",
+        notes:notes
+    })
+})
+
+app.delete("/notes/:index", (req,res)=>{
+    const index = req.params.index;
+    delete notes [index]
+    res.status(200).json({
+        message:"Note is successfully deleted"
+    })
+
+})
+
+app.patch("/notes/:index", (req, res) => {
+    const index = req.params.index;
+    const title = req.body.title
+    const description = req.body.description
+
+    notes[ index ].title = title
+    notes[ index ].description = description
+
+    res.status(200).json({
+        message:"Note is successfully updated"
+    })
+
 })
